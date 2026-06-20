ed585ac528e5e041e7226e4ed015761df7f32b5f
zenith <antuanzuniga846@gmail.com>
2026-06-20 14:14:40 -0600

    Update

---
+++ js/plantillas.js
@@ -0,0 +1,31 @@
+const plantillas = {
+  nc: `
+    <h3>Plantilla Nota de Crédito</h3>
+    <textarea style="width:100%;height:300px;">
+200_Recuperacion de Clientes Proyecto de Ventas Móvil
+
+200_ Se aplica limpieza de saldos por el monto de
+VB Operaciones Comerciales
+
+200_Se aplico reversión por proyecto de Venta Servicio Móvil Limpieza de Saldos
+
+Se procede para la reversión de nota de crédito.
+    </textarea>
+  `,
+
+  autorizacion: `
+    <h3>Plantilla Autorización</h3>
+    <textarea style="width:100%;height:300px;">
+Se procede con la autorización, por favor validar, cualquier consulta adicional quedamos a su disposición.
+
+Se solicita prioridad, cualquier consulta adicional quedamos a su disposición.
+
+Compañeros su apoyo dando prioridad y autorización a la orden en CRM.
+    </textarea>
+  `
+};
+
+function mostrarPlantilla(tipo){
+  document.getElementById("contenidoPlantilla").innerHTML =
+    plantillas[tipo] || "Plantilla no encontrada";
+}
